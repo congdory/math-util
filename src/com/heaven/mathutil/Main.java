c729075cbe2a0c2f4b04e39b618efba8c3c46b44
congdory <chicong0902499598@gmail.com>
2021-03-25 16:28:00 +0700

    Update core code

--- src/com/heaven/mathutil/Main.java
+++ src/com/heaven/mathutil/Main.java
@@ -13,18 +13,22 @@ public class Main {
 
     //at here, it is class version-copy of Math, it has all function help for math
     public static final double PI = 3.1415;
-
+    
     public static long getFactorial(int n) {
-        long result = 1;
+//        long result = 1;
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be between 0..20");
         }
         if (n == 0 || n == 1) {
             return 1;
         }
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        return n * getFactorial(n - 1);
+        
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
+        
     }
 }
