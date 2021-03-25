5ae03247274482271e426f0f7fdbe4e2f81c3313
congdory <chicong0902499598@gmail.com>
2021-03-25 15:44:06 +0700

    Update core code

--- src/com/heaven/mathutil/Main.java
+++ src/com/heaven/mathutil/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public static final double PI = 3.1415;
 
     public static long getFactorial(int n) {
-        long result = 0;
+        long result = 1;
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be between 0..20");
         }
